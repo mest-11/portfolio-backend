5145384d413cea38ff363e7ada9b30c4c15a3517
caroline-joyiee <joycmortty@gmail.com>
2024-07-16 10:01:13 +0000

    uploads

--- routers/achievement-route.js
+++ routers/achievement-route.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { deleteAchievement, getAchievement, patchAchievement, postAchievement } from "../controllers/achievement-controller.js";
 import { checkUserSession } from "../middleware/auth.js";
+import { remoteUpload } from "../middleware/upload.js";
 
 export const achievementRouter = Router()
 
