c68b3e8687bab7027acb54c49285984b629b2b80
menniia <mennia17@gmail.com>
2024-07-29 16:10:32 +0000

    fixed changes

--- index.js
+++ index.js
@@ -30,8 +30,7 @@ expressOasGenerator.handleResponses(app, {
 
 // apply middlewares
 app.use(express.json());
-// app.use(cors({ credentials: true, origin: "*" }))
-app.use(cors({ credentials: true, origin: "http://localhost:5173" }));
+app.use(cors({ credentials: true, origin: "*" }))
 
 
 app.use(session({
